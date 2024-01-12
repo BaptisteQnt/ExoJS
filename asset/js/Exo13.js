f3bc0b7c263623b9bade6aa0ae970c6eae962f61
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-01-12 11:28:53 +0100

    Exo4 first resolution

--- asset/js/Exo13.js
+++ asset/js/Exo13.js
@@ -1,3 +1,9 @@
 const click = document.querySelector('.img1').addEventListener('click',function zoom(){
-    
+   document.querySelector('#XXL').classList.add('img1xxl')
 }) 
+
+const click1 = document.querySelector('.img2').addEventListener('click',function zoom(){
+    document.querySelector('#XXL').classList.add('img2xxl')
+ }) 
+
+//  Et on fais ça pour toute les images a zommer lors d'un click.
