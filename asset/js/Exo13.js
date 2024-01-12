2b56f308ace1fafe8c5c34ce1061eebe590f3942
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-01-12 13:43:07 +0100

    exo 4 correction

--- asset/js/Exo13.js
+++ asset/js/Exo13.js
@@ -1,9 +1,35 @@
-const click = document.querySelector('.img1').addEventListener('click',function zoom(){
-   document.querySelector('#XXL').classList.add('img1xxl')
-}) 
+// const click = document.querySelector('.img1').addEventListener('click',function zoom(){
+//    document.querySelector('#XXL').classList.add('img1xxl')
 
-const click1 = document.querySelector('.img2').addEventListener('click',function zoom(){
-    document.querySelector('#XXL').classList.add('img2xxl')
- }) 
+//    document.querySelector('.img').addEventListener('click', function reset(){
+//     document.querySelector('#XXL').classList.remove('img1xxl');
+//    })
+// }) 
 
-//  Et on fais ça pour toute les images a zommer lors d'un click.
+// const click1 = document.querySelector('.img2').addEventListener('click',function zoom(){
+//     document.querySelector('#XXL').classList.add('img2xxl')
+
+//     document.querySelector('.img').addEventListener('click', function reset(){
+//     document.querySelector('#XXL').classList.remove('img2xxl');
+//  }) 
+// })
+// //  Et on fais ça pour toute les images a zoommer lors d'un click.
+
+let smallImages = document.querySelectorAll('.small');
+let bigImage = document.querySelector('.big');
+let saveImage = smallImages[0];
+
+smallImages.forEach(image => {
+  image.addEventListener('click', displayImage);
+});
+
+function displayImage(event) {
+  bigImage.src = event.target.src;
+  saveImage.classList.remove('focus');
+  saveImage = event.target;
+
+  // smallImages.forEach(image => {
+  //   image.classList.remove('focus');
+  // });
+  event.target.classList.add('focus');
+};
